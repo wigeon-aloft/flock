46138d5af84c3f9fabd8e1df9242715ce373e5c6
Willem van Hoof <willem@thevanhoofs.com>
2022-08-29 17:26:03 +0100

    Updated command dict with more in-depth information and completed help method functionality. Added 'usage' guides for each command Added long and short descriptions of each command's function Finished implementing 'help' command

--- src/bot.py
+++ src/bot.py
@@ -29,7 +29,11 @@ class FlockClient(discord.Client):
                     "h"
                 ],
                 "response": "Available commands:\n{}",
-                "description": ""
+                "description": {
+                    "short": "Command usage information",
+                    "long": "Provide information for a particular command. Pass the name of a command as the first positional argument. If none is provided a list of available commands will be displayed."
+                },
+                "usage": "{} {} <command_name>"
             },
             "add": {
                 "name": "add",
@@ -41,7 +45,11 @@ class FlockClient(discord.Client):
                     "a"
                 ],
                 "response": "Added {} to {} queue.",
-                "description": "Add a user to an existing queue."
+                "description": {
+                    "short": "Add a user to a queue",
+                    "long": "Add a user to an existing queue. If no queue name specified then user will be added to most recent/only queue if one exists. If no user is specified then the message sender will be added."
+                },
+                "usage": "{} {} <queue_name> <user>"
             },
             "create": {
                 "name": "create",
@@ -51,7 +59,11 @@ class FlockClient(discord.Client):
                     "c"
                 ],
                 "response": "Created queue {}.",
-                "description": "Creates a new queue."
+                "description": {
+                    "short": "Create a new queue",
+                    "long": "Create a new queue. Pass a name for the queue as the first positional argument, otherwise one will be randomly generated."
+                },
+                "usage": "{} {} <queue_name>"
             },
             "show": {
                 "name": "show",
@@ -61,7 +73,11 @@ class FlockClient(discord.Client):
                     "s"
                 ],
                 "response": "{} you are in {}.",
-                "description": "Lists the queues the user is current in."
+                "description": {
+                    "short": "Show queues that a user is in",
+                    "long": "Show queues that a specified user is in. Pass a username/ID as the first positional argument."
+                },
+                "usage": "{} {} *<user>"
             },
             "leave": {
                 "name": "leave",
@@ -73,17 +89,27 @@ class FlockClient(discord.Client):
                     "r"
                 ],
                 "response": "{} removed from {}",
-                "description": "Removes a user from a queue."
+                "description": {
+                    "short": "Remove a user from a queue",
+                    "long": "Remove a user from a queue. If no user is specified then the message sender will be removed."
+                },
+                "usage": "{} {} *<queue_name> <user>"
             },
             "status": {
                 "name": "status",
                 "method": self.status,
                 "trigger": [
                     "status",
-                    "t"
+                    "t",
+                    "list",
+                    "ls"
                 ],
                 "response": "Current members of the {} queue:\n{}",
-                "description": "Shows the current members of a queue."
+                "description": {
+                    "short": "Show queue information",
+                    "long": "Show information for a specified queue. Provide queue name as the first positional argument. If no queue name is provided then it will show a list of existing queues."
+                },
+                "usage": "{} {} <queue_name>"
             },
             "delete": {
                 "name": "delete",
@@ -94,7 +120,11 @@ class FlockClient(discord.Client):
                     "d"
                 ],
                 "response": "Deleted {} queue.",
-                "description": "Deletes an existing queue."
+                "description": {
+                    "short": "Delete an existing queue",
+                    "long": "Delete an existing queue. Provide a queue name as the first positional argument."
+                },
+                "usage": "{} {} *<queue_name>"
             }
         }
 
@@ -168,15 +198,32 @@ class FlockClient(discord.Client):
             
     async def help(self, message, args):
         """Prints help information for the given command or shows a list of commands if command not provided."""
+        
+        # Generate a string containing information for each available command 
+        command_help_string = '\n'.join(
+            [f"**{command.get('name')}** - {command.get('description').get('short')}" for command in dict(sorted(self._commands.items())).values()])
+
+        # If no command query was provided, show a list of available commands
+        if len(args) == 0:
+            await message.channel.send(self._commands.get("help").get("response").format(command_help_string))
+            return
 
         try:
             command = self.get_command_from_trigger(args[0])
-            await message.channel.send(f"{command} - {command.get('description')}")
+
+            # Command exists - show help message for the command
+            trigger_usage_string = f"{{ {' | '.join(command.get('trigger'))} }}"
+            help_message = f"**{', '.join(command.get('trigger'))}**\n```{command.get('usage').format(self._trigger_phrase, trigger_usage_string)}```\n> {command.get('description').get('long')}"
+
+            # Add information on required arguments if there are any in the command usage information string
+            if '*' in command.get('usage'):
+                help_message += "\n*Note: parameters preceded by a '\*' are required.*"
+
+            await message.channel.send(help_message)
 
         except KeyError:
             # Command doesn't exist - show a general list of commands and associated triggers
-            command_help_string = '\n'.join([f"{command.key()} - {command.get('description')}" for command in self._commands])
-            await message.channel.send(f"{args[0]} is not a valid command. Below is a list of available commands:\n{command_help_string}")
+            await message.channel.send(f"`{args[0]}` not recognised. {self._commands.get('help').get('response').format(command_help_string)}")
     
     async def create(self, message, args):
         # Get the user-specified queue name, if provided
